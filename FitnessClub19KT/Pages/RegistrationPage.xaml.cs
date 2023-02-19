1f744fe1e7179e4a85bcbd63895c8095bb8bf5e3
Popcika <aleksandr.korchevnyy.67@mail.ru>
2023-02-20 00:27:13 +0300

    RAC 1.2

--- FitnessClub19KT/Pages/RegistrationPage.xaml.cs
+++ FitnessClub19KT/Pages/RegistrationPage.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 
 using FitnessClub19KT.DB;
 using FitnessClub19KT.Pages;
+using FitnessClub19KT.Windows;
 using FitnessClub19KT.ClassHelper;
 
 namespace FitnessClub19KT.Pages
@@ -27,11 +28,53 @@ namespace FitnessClub19KT.Pages
         public RegistrationPage()
         {
             InitializeComponent();
+            CmbGender.ItemsSource = ClassHelper.EFClass.context.Gender.ToList();
+            CmbGender.DisplayMemberPath = "Title";
+            CmbGender.SelectedIndex = 0;
+
         }
 
-        private void btnSignUn_Click(object sender, RoutedEventArgs e)
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
+            if (string.IsNullOrWhiteSpace(TbFirstName.Text))
+            {
+                MessageBox.Show("Поле \"Фамилия\" не может быть пустым.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            if(string.IsNullOrWhiteSpace(TbLastName.Text))
+            {
+                MessageBox.Show("Поле \"Имя\" не может быть пустым.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(TbPhone.Text))
+            {
+                MessageBox.Show("Поле \"Телефон\" не может быть пустым.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(TbEmail.Text))
+            {
+                MessageBox.Show("Поле \"Электронная почта\" не может быть пустым.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            if (string.IsNullOrWhiteSpace(DpBirthday.Text))
+            {
+                MessageBox.Show("Поле \"Дата рождения\" не может быть пустым.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
 
+            Client client = new Client();
+
+            client.FirstName = TbFirstName.Text;
+            client.LastName = TbLastName.Text;
+            client.Patronymic = TbPatronymic.Text;
+            client.BirthdayDate = DpBirthday.SelectedDate.Value; 
+            client.Phone= TbPhone.Text;
+            client.IdGender = (CmbGender.SelectedItem as Gender).IdGender;
+
+            ClassHelper.EFClass.context.Client.Add(client);
+
+            ClassHelper.EFClass.context.SaveChanges();
+            MessageBox.Show("Ок");
         }
     }
 }
